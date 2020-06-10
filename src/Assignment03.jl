6bfe0fe3384d5ff6a218ffe4714a5e447867db0a
Kevin Bonham <kevbonham@gmail.com>
2020-06-10 18:04:26 -0400

    write tests

--- src/Assignment03.jl
+++ src/Assignment03.jl
@@ -1,14 +1,11 @@
 module Assignment03
 
-export question1,
-       question2,
-       question3,
-       question4,
-       question5
-
-# using BISC195
+export compliment,
+       ispurine,
+       ispyrimidine,
+       base_type,
+       gc_content
 
 include("assignment.jl")
-# Write your package code here.
 
 end
